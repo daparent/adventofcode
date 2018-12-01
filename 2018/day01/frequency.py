5b1a01315c9a31e0096e2cc759d6752c2ebf986a
Notorious D.A.P <daparent@gmail.com>
2018-12-01 14:35:19 -0500

    Code for day 1 of Advent of Code 2018
    Is there a better way?  Probably but this is a little less brute force
    than creating a list of every value and constantly doing an "in" check
    on it.

---
+++ 2018/day01/frequency.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+def frequencyChange(cur_frequency, frequency_change):
+    modifier = frequency_change[0]
+    change_amount = int(frequency_change[1:])
+    if modifier == '-':
+        cur_frequency -= change_amount
+    else:
+        cur_frequency += change_amount
+    return cur_frequency
+
+
+def main():
+    print("Calculating frequency, hold on to your butts...")
+    cur_frequency = 0
+    iterations = 1
+    calculated_frequencies = {}
+    repeated_frequency = -1
+    frequency_changes = []
+    with open('input.txt', 'r') as inputfile:
+        frequency_changes = inputfile.readlines()
+    while repeated_frequency == -1:
+        for line in frequency_changes:
+            cur_frequency = frequencyChange(cur_frequency, line)
+            if repeated_frequency == -1:
+                if calculated_frequencies.get(cur_frequency) is not None:
+                    repeated_frequency = cur_frequency
+                else:
+                    calculated_frequencies[cur_frequency] = True
+        if iterations == 1:
+            print("Calculated Frequency is {}".format(cur_frequency))
+        iterations = iterations + 1
+    print("Repeated frequncy is {}".format(repeated_frequency))
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # adventofcode
 Advent of code... code
+
+My attempt at answering the puzzle questions for the Advent of Code.
+I'll be using a language I don't use regularly so if you look at what
+I've done, scratch your head and/or laugh, well, take it for what it is.
+
+For 2018 I'm going to use python
